afe266df3a2ed0409177e305969c60ea9de59d4f
Benjamin Vredevelt <bjv753@gmail.com>
2023-08-17 15:09:16 -0500

    portfolio update

--- react-app-template-1/f/src/components/ProtectedRoute.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Route, Navigate, Outlet } from 'react-router-dom'
-
-export default function ProtectedRoute(props){
-  const { token, redirectTo, children } = props
-  return token ? children : <Navigate to={redirectTo}/> 
-}
